0be412636da4e3e12d1354f1873cb2dd23728a65
root <root@BeagleBoard-X15.localdomain>
2016-04-23 05:36:58 +0000

    add China apt source list

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -104,7 +104,7 @@ check_defines () {
 	if [ ! "${deb_mirror}" ] ; then
 		case "${deb_distribution}" in
 		debian)
-			deb_mirror="httpredir.debian.org/debian/"
+			deb_mirror="ftp.cn.debian.org/debian/"
 			;;
 		ubuntu)
 			deb_mirror="ports.ubuntu.com/ubuntu-ports/"
